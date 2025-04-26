96fa4c3e28dc70863f85b5288fb7710d8cc72a7d
thewoowon <thewoowon@naver.com>
2025-04-26 22:43:15 +0900

    refactor: banner components changed

--- components/module/Banner/assets/Banner1.tsx
+++ components/module/Banner/assets/Banner1.tsx
@@ -14,7 +14,7 @@ const Container = styled.div`
   position: relative;
   border-radius: 12px;
   box-shadow: 0px 3px 20px 0 rgba(0, 0, 0, 0.04);
-  background-image: url("/assets/images/purification_banner.png");
+  background-image: url("/assets/images/purification.png");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
--- components/module/Banner/assets/index.ts
+++ components/module/Banner/assets/index.ts
@@ -1,2 +1,3 @@
 export { default as Banner1 } from "./Banner1";
 export { default as Banner2 } from "./Banner2";
+export { default as BannerClient } from "./BannerClient";
