1d76294e06c63cd45aebb8a2a588469e2a8feaa8
thewoowon <thewoowon@naver.com>
2025-04-26 11:51:57 +0900

    refactor: not found page edited

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,33 +1,48 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 'calc(var(--vh, 1vh) * 100)',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "calc(var(--vh, 1vh) * 100)",
       }}
     >
       <div
         style={{
-          width: '224px',
-          height: '143px',
-          position: 'relative',
+          width: "224px",
+          height: "143px",
+          position: "relative",
         }}
       >
-        <Image src={'/images/error/network-error.png'} alt="" priority fill />
+        <Image
+          src={"/assets/images/error/network-error.png"}
+          alt=""
+          priority
+          fill
+        />
       </div>
-      <div>존재하지 않는 경로에요 🤣</div>
+      <div
+        style={{
+          fontSize: "24px",
+          fontWeight: "semibold",
+          marginTop: "16px",
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        존재하지 않는 경로에요 🤣
+      </div>
       <Link
         href="/"
         style={{
-          marginTop: '16px',
-          color: '#0070f3',
-          textDecoration: 'none',
+          marginTop: "16px",
+          color: "#920000",
+          textDecoration: "none",
         }}
       >
         홈으로 가기
